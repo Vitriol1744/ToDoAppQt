39dea63a4ba4b6a03fddcefa61669ee0e10b9fb3
V1tr10l7 <vitriol1744@gmail.com>
2024-05-12 13:36:36 +0200

    Initial commit

---
+++ src/main.cc
@@ -0,0 +1,15 @@
+/*
+ * Created by vitriol1744 on 12.05.2024.
+ * Copyright (c) 2024-2024, Szymon Zemke <v1tr10l7@proton.me>
+ *
+ * SPDX-License-Identifier: GPL-3
+ */
+#include <cstdlib>
+#include <cstdio>
+
+int main(int argc, char** argv)
+{
+    printf("Hello, World!\n");
+
+    return EXIT_SUCCESS;
+}
